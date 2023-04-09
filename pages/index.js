3fe30f3b4568d255d77c47cf028aaf0430893f8b
Prathamesh48 <prathameshpatil6499@gmail.com>
2023-04-09 15:44:34 +0530

    bug:solved

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY }`,
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY }`,
       },
       body: JSON.stringify({
         prompt: `${input} Translate this text to ${selectedLanguage}`
